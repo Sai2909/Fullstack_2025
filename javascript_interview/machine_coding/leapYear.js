29f61bfb45cb96e04c8a028941772751af02c127
saikiran <saikiranmanchala2000@gmail.com>
2024-12-17 23:36:48 +0530

    leapyear

---
+++ javascript_interview/machine_coding/leapYear.js
@@ -0,0 +1,21 @@
+function isLeapYear(year) {
+    if (year % 4 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function isLeapYear2(year) {
+    if (year % 4 === 0) {
+        return true
+    }
+    return false
+}
+
+function isLeapYear3(year) {
+    return year % 4 === 0
+}
+
+console.log("isLeapYear(2023)", isLeapYear(2023))
+console.log("isLeapYear(2024)", isLeapYear(2024))
