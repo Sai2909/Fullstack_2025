aafe632daf7b498f0872d303f4ecc8befab64a1e
saikiran <saikiranmanchala2000@gmail.com>
2025-06-02 15:11:42 +0530

    map

---
+++ javascript_interview/new things latest/map.js
@@ -0,0 +1,6 @@
+/*
+The map() method of an array can be used to transform the elements of an array  by applaying
+A callback function to each element of the source array.
+=>it returns a new array with the transformed data.
+=>applies a function callback to each el
+*/
